23c69feb82be32927403ef0db552a4c358237e1a
Shubham <byshubham6@gmail.com>
2024-05-04 21:44:05 +0530

    #

---
+++ music_app/urls.py
@@ -0,0 +1,34 @@
+from django.urls import path, include, re_path
+from . import views as music_streaming_views
+from django.contrib.auth.views import LoginView, LogoutView
+
+urlpatterns = [
+    #home /
+    path('', music_streaming_views.home, name='home'),
+
+    #profile_detail /@username/
+    path('@<str:username>/',music_streaming_views.profile_detail, name='profile_detail'),
+
+    #add new album /@username/add
+    path('@<str:username>/add/', music_streaming_views.add_album, name='add_album'),
+
+    #album's detail page /@username/album/album_name
+    path('@<str:username>/album/<str:album>/', music_streaming_views.album_detail, name='album_detail'),
+
+    # login the user /login/
+    path('login/', LoginView.as_view(template_name='music_nation/user_login.html'), name="login"),
+
+    # signUp new user /signup/
+    path('signup/', music_streaming_views.signup, name='signup'),
+
+    #delete album /@username/album/album_name/delete
+    path('@<str:username>/album/<str:album>/delete/', music_streaming_views.delete_album, name='delete_album'),
+
+    #add songs to the albums
+    path('@<str:username>/album/<str:album>/add/', music_streaming_views.add_song, name='add_song'),
+
+    #logout the current user
+    path('logout/', LogoutView.as_view(), name='logout'),
+]
+#path('link', view, name='', kwargs={})
+#re_path(r'regex', view, name='', kwargs={})
